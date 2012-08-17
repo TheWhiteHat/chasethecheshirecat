7aa7d1f42e806a3b2e5589722ee32ba35185de37
VeryBueno <lcuellar@calpoly.edu>
2012-08-16 22:36:31 -0700

    modified settings for clarity

--- chasethecheshirecat/settings.py
+++ chasethecheshirecat/settings.py
@@ -49,7 +49,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/luis/Projects/chasethecheshirecat/media/'
+MEDIA_ROOT = 'media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/luis/Projects/chasethecheshirecat/static/'
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
